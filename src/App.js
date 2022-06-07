f5b7fa6bb00310e2bc820b8d9efba4deb5d55218
Damian Oportus <coportus99@gmail.com>
2022-06-06 20:29:33 -0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import * as React from 'react';
 import Homepage from "./pages/Homepage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ResponsiveAppBar from "./components/NavBar";
 
 
 
 function App() {
 
   return ( 
-  <>
-  <Homepage/>
-  </>
+  <Router>
+    <>
+    <ResponsiveAppBar/>
+    <Routes>
+      <Route path='/' element={<Homepage/>} />
 
+    </Routes>
+    </>
 
+  </Router>
   );
 }
 
