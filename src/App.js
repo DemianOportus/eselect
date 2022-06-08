056f98313d48c292f4bcdc1aead1b265561610d8
Damian Oportus <coportus99@gmail.com>
2022-06-08 14:37:01 -0400

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import * as React from 'react';
+import React from "react";
 import Homepage from "./pages/Homepage";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ResponsiveAppBar from "./components/NavBar";
+// const express = require("express");
+// const bodyParser = require("body-parser");
+
 
 
 
@@ -22,3 +25,59 @@ function App() {
 }
 
 export default App;
+
+
+// //jshint esversion:8
+
+// // "mongodb+srv://oportus:mongodb321.@projecttest.kiiv0.mongodb.net/myUsers?retryWrites=true&w=majority"
+// const mongoose = require('mongoose');
+
+// //connect to MongoDB by specifying port to access MongoDB server
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb://localhost:27017/myUsers', {useNewUrlParser: true});
+//   }// creates new Database if not exist named myUsers.
+
+// const userSchema = new mongoose.Schema({
+//   name:String,
+//   password:String
+// }); // define how to our collection will look alike.
+
+// const User =  mongoose.model("User", userSchema); //
+// const userNew = new User  ({
+//     name: "John",
+//     password: "123"
+// })
+// const userNew2 = new User  ({
+//   name: "Mark",
+//   password: "321"
+// })
+// const userNew3 = new User  ({
+//   name: "Luke",
+//   password: "222"
+// })
+// const defaultUsers = [userNew, userNew2, userNew3]
+
+// User.insertMany(defaultUsers, function(err) {
+//   err ? console.log(err) : console.log("Successfully saved default users to DB")
+//   })
+
+// // const singupTemplate = new mongoose.Schema({
+// //     userName: {
+// //         type: String,
+// //         required: true
+// //     },
+// //     email: {
+// //         type: String,
+// //         required: true
+// //     },
+// //     password: {
+// //         type: String,
+// //         required: true
+// //     },
+// //     date: {
+// //         type: Date,
+// //         default: Date.now
+// //     }
+// // })
