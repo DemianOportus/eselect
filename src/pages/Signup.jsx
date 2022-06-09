fc571fd155c48056fbed5118acae1549bd7b37a2
Monica Oportus <monicaoportus@Monicas-MacBook-Air.local>
2022-06-09 09:57:20 -0400

    error4 + seperated login/signup + navbar update

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,22 @@
+import BlackNavbar from "../components/blackNavbar";
+
+function Signup(){
+    return (
+      <div className="loginPage">
+        <BlackNavbar />
+          <div className="loginText"> 
+            <p className="welcome">Join us today!</p>
+            <h1>Sign up for free</h1>
+            <input className="loginInput" placeholder="Username"></input>
+            <input className="loginInput" placeholder="Password"></input>
+            <input className="loginInput" placeholder="Confirm password"></input>
+            <button className="loginButton">Sign up</button>
+            <div style={{paddingTop: "17px"}}>
+                <a className="loginLink" href="/login">Login</a>
+            </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default Signup;
