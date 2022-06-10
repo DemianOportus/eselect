27607f28a9908a30039d9ebdfe98978721265fc4
Damian Oportus <coportus99@gmail.com>
2022-06-10 11:47:28 -0400

    Refactor backend

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,16 +1,56 @@
 import BlackNavbar from "../components/blackNavbar";
+import React from "react";
+import { useState } from "react";
+
+function createUser(username, password, confirmPassword) {
+  fetch('/users', {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+      password: password,
+      confirmPassword: confirmPassword
+      
+  }),
+  headers: {
+    "Content-Type": "application/json"
+}
+  
+  })
+  .then(response => response.text())
+  .then(data => alert(data));
+}
+
 
 function Signup(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
     return (
       <div className="loginPage">
         <BlackNavbar />
           <div className="loginText"> 
             <p className="welcome">Join us today!</p>
             <h1>Sign up for free</h1>
-            <input className="loginInput" placeholder="Username"></input>
-            <input className="loginInput" placeholder="Password"></input>
-            <input className="loginInput" placeholder="Confirm password"></input>
-            <button onClick={() => alert("Hola")} className="loginButton">Sign up</button>
+            <input 
+              onChange={(event) => setUsername(event.target.value)} 
+              value={username} 
+              className="loginInput" 
+              placeholder="Username"
+            />
+            <input 
+              onChange={(event) => setPassword(event.target.value)} 
+              value={password} 
+              className="loginInput" 
+              placeholder="Password"/>
+
+            <input 
+              onChange={(event) => setConfirmPassword(event.target.value)} 
+              value={confirmPassword} 
+              className="loginInput" 
+              placeholder="Confirm password"/>
+
+            <button className="loginButton" onClick={ () => createUser(username, password, confirmPassword) }>Sign up</button>
             <div style={{paddingTop: "17px"}}>
                 <a className="loginLink" href="/login">Login</a>
             </div>
