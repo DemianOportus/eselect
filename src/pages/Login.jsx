1e6c6cf7db797b1cad665f265a3c85dfa381f7f9
Damian Oportus <coportus99@gmail.com>
2022-06-22 11:35:14 -0400

    Making it better

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,67 @@
 import BlackNavbar from "../components/blackNavbar";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+var isLoggedIn = false;
+
+function verifyUser(email, password, navigate) {
+  fetch("/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw response;
+      }
+      isLoggedIn = true;
+      return response;
+    })
+    .then(() => navigate("/"))
+    .catch((response) => response)
+    .then((response) => response.text())
+    .then((data) => alert(data));
+}
 
 function Login() {
-
-      return (
-        <div className="loginPage">
-          <BlackNavbar />
-          <div className="loginText">
-            <p className="welcome">Welcome back!</p>
-            <h1>Sign in to e-selection</h1>
-            <input className="loginInput" placeholder="Username"></input>
-            <input className="loginInput" placeholder="Password"></input>
-            <button className="loginButton">Login</button>
-            <div style={{paddingTop: "17px"}}>
-              <a className="signupLink" href="/signup">Sign up</a>
-            </div>
-          </div>
+  const navigate = useNavigate();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  return (
+    <div className="loginPage">
+      <BlackNavbar />
+      <div className="loginText">
+        <p className="welcome">Welcome back!</p>
+        <h1>Sign in to e-selection</h1>
+        <input
+          onChange={(event) => setEmail(event.target.value)}
+          className="loginInput"
+          placeholder="Email"
+        ></input>
+        <input
+          onChange={(event) => setPassword(event.target.value)}
+          className="loginInput"
+          placeholder="Password"
+        ></input>
+        <button
+          onClick={() => verifyUser(email, password, navigate)}
+          className="loginButton"
+        >
+          Login
+        </button>
+        <div style={{ paddingTop: "17px" }}>
+          <a className="signupLink" href="/signup">
+            Sign up
+          </a>
         </div>
-      );
-  }
-  
-  export default Login;  
+      </div>
+    </div>
+  );
+}
+
+export default Login;
